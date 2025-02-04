e9cab6ee546ddeb3fb70da1994a91eb39f360448
Bast <bastian.penaloza@genstudents.org>
2025-02-04 10:36:12 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function calcularFactorial() {
+    let input = document.getElementById('numero').value;
 
+    let numero = Number(input);
+
+    if (isNaN(numero)) {
+        alert("Error: Debes ingresar un número válido.");
+        return;
+    }
+
+    let factorial = 1;
+    for (let i = 1; i <= numero; i++) {
+        factorial *= i;
+    }
+
+    document.getElementById('resultado').innerText = `El factorial de ${numero} es: ${factorial}`;
+}
